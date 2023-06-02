5ddb04c59fd2f61efddb2e99ebfb399a5acb96bf
JavierAcosta <a6521150022@utch.edu.mx>
2023-06-01 22:58:19 -0600

    Maquetacion Map, buy y home

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
+
+app.get('/buy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'buy.html'));
+  });
   
   app.get('/auth', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'auth.html'));
